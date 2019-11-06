18de20f1635de5824212124a9988b4a291ccde00
kwojewodzki <45183401+kwojewodzki@users.noreply.github.com>
2019-11-06 21:13:58 +0100

    Add files via upload

---
+++ selection.h
@@ -0,0 +1,35 @@
+//
+// Created by arsze on 04.11.2019.
+//
+
+#ifndef PROGRAMING_AISD_TEMPLATE_H
+#define PROGRAMING_AISD_TEMPLATE_H
+
+
+//
+// Created by KejDablju on 04.11.2019.
+//
+
+using namespace std;
+
+template<typename IteratorType>
+void bubbleSort(IteratorType first, IteratorType last)
+{
+    IteratorType a = first;
+    IteratorType b = first;
+    IteratorType mini = first; //counter
+    while(a < last){
+        mini = a;
+        b = a;
+        while (b < last){
+            if( *b < *mini){
+                mini = b;
+            }
+            ++b;
+        }
+        swap (*mini, *a);
+        ++a;
+    }
+}
+
+#endif //PROGRAMING_AISD_TEMPLATE_H
