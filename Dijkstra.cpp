39f18b820d885fc0643e33bafffd6711d49cb1cf
kwojewodzki <45183401+kwojewodzki@users.noreply.github.com>
2019-12-20 09:11:35 +0100

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,3 +1,32 @@
+int i, j, n, v, u, sptr, *d, *p, *S;
+    bool *QS;                       // Zbiory Q i S
+    slistEl **graf;                 // Tablica list sąsiedztwa
+    slistEl *pw, *rw;
+    n = 35;
+    v = 0;
+              // Węzeł startowy, liczba wierzchołków i krawędzi
+
+    // Tworzymy tablice dynamiczne
+
+    d = new int [ n ];                // Tablica kosztów dojścia
+    p = new int [ n ];                // Tablica poprzedników
+    QS = new bool [ n ];              // Zbiory Q i S
+    graf = new slistEl * [ n ];       // Tablica list sąsiedztwa
+    S = new int [ n ];                // Stos
+    sptr = 0;                       // Wskaźnik stosu
+
+    // Inicjujemy tablice dynamiczne
+
+    for(i = 0; i < n; i++)
+    {
+        d [ i ] = MAXINT;
+        p [ i ] = -1;
+        QS [ i ] = false;
+        graf [ i ] = NULL;
+    }
+
+    // Odczytujemy dane wejściowe
+
     int k = 0;
     int tab_val [6] [6];
     int tab_num [6] [6];
